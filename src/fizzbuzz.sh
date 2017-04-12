a3d6f1d13273a0fdb2cfb1990daeddcd813fa15f
Tristan <tristan.roussel@epita.fr>
2017-04-12 14:53:08 +0100

    Fizzbuzzify a range of integers with the use of a default number (to).

--- src/fizzbuzz.sh
+++ src/fizzbuzz.sh
@@ -5,6 +5,7 @@ fizzbuzz() {
         +([0-9]))
         numbers=($@)
         fizzbuzzifyOnList ${numbers[@]}
+        return
         ;;
         --from | -f)
         case $2 in
@@ -15,6 +16,7 @@ fizzbuzz() {
                     +([0-9]))
                     numbers=($(seq $2 $4))
                     fizzbuzzifyOnList ${numbers[@]}
+                    return
                     ;;
                 esac
                 ;;
@@ -22,23 +24,21 @@ fizzbuzz() {
             ;;
         esac
         ;;
+        --to | -t)
+        case $2 in
+            +([0-9]))
+            numbers=($(seq 1 $2))
+            fizzbuzzifyOnList ${numbers[@]}
+            return
+            ;;
+        esac
+        ;;
         * | --help | -h)
-        echo "fizzbuzz.sh -- Fizzbuzzify one or more integers, or a range of integers"
-        echo "  fizzbuzz <number>..."
-        echo "  fizzbuzz [OPTIONS]"
-        echo ""
-        echo "  -f, --from START"
-        echo "      Fizzbuzzify a range begginning at the START value."
-        echo "      This option must be used with the '--to' option."
-        echo "      Optional. If the END parameter is given, START is valued at 1 by default."
-        echo "  -h, --help"
-        echo "      Display the help."
-        echo "  -t, --to END"
-        echo "      Fizzbuzzify a range ending at the END value."
-        echo "      This option may be used with the '--from' option."
-        echo "      Conditional. This option is required if the '--from' option is used."
+        displayHelp
+        return
         ;;
     esac
+    displayHelp
 }
 
 fizzbuzzify() {
@@ -60,4 +60,22 @@ fizzbuzzifyOnList() {
         out="$out`fizzbuzzify $index` "
     done
     echo ${out}
+}
+
+displayHelp() {
+    echo "fizzbuzz.sh -- Fizzbuzzify one or more integers, or a range of integers"
+    echo "  fizzbuzz <number>..."
+    echo "  fizzbuzz [OPTIONS]"
+    echo ""
+    echo "  -f, --from START"
+    echo "      Fizzbuzzify a range begginning at the START value."
+    echo "      This option must be used with the '--to' option."
+    echo "      Optional. If the END parameter is given, START is valued at 1 by default."
+    echo "  -h, --help"
+    echo "      Display the help."
+    echo "  -t, --to END"
+    echo "      Fizzbuzzify a range ending at the END value."
+    echo "      This option may be used with the '--from' option."
+    echo "      Conditional. This option is required if the '--from' option is used."
+ 
 }
