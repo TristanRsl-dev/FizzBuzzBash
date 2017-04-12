9ce7397e4bb2c0157881058866d9a2eea2385b67
Tristan <tristan.roussel@epita.fr>
2017-04-12 13:46:18 +0100

    Fizzbuzzify a number from the command line.

--- src/fizzbuzz.sh
+++ src/fizzbuzz.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+fizzbuzz() {
+    for arguments in "$@"
+    do
+        case $arguments in
+            +([0-9]))
+            fizzbuzzify ${arguments}
+            ;;
+            --help | -h)
+            echo "You ask for the help prompt"
+            ;;
+        esac
+    done
+}
+
 fizzbuzzify() {
-    if !(($1 % 3)) && !(($1 % 5))
-        then echo "FizzBuzz"
-    elif !(($1 % 3))
-        then echo "Fizz"
-    elif !(($1 % 5))
-        then echo "Buzz"
+    if (($1 % 3 == 0)) && (($1 % 5 == 0)); then
+        echo "FizzBuzz"
+    elif (($1 % 3 == 0)); then
+        echo "Fizz"
+    elif (($1 % 5 == 0)); then
+        echo "Buzz"
         else echo "$1"
     fi
 }
