82e14a51830f7569a6cd958d763875f83e945f7f
Tristan <tristan.roussel@epita.fr>
2017-04-14 11:57:49 +0100

    Refactor to delete commands from the suggestion line when they are used.

--- src/autocompletion.sh
+++ src/autocompletion.sh
@@ -5,7 +5,7 @@ autocomplete() {
     COMPREPLY=()
     current="${COMP_WORDS[COMP_CWORD]}"
     previous="${COMP_WORDS[COMP_CWORD-1]}"
-    options="--help --from --to -h -f -t"
+    options=("--from" "-f" "--to" "-t" "--help" "-h")
 
     case "${previous}" in
         --from | -f | --to | -t)
@@ -13,7 +13,17 @@ autocomplete() {
         ;;
     esac
 
-    COMPREPLY=($(compgen -W "${options}" -- ${current}))
+    if [[ "${COMP_WORDS[*]}" == *"-f"* ]] || [[ "${COMP_WORDS[*]}" == *"--from"* ]]; then
+        unset options[0]
+        unset options[1]
+    fi
+
+    if [[ "${COMP_WORDS[*]}" == *"-t"* ]] || [[ "${COMP_WORDS[*]}" == *"--to"* ]]; then
+        unset options[2]
+        unset options[3]
+    fi
+
+    COMPREPLY=($(compgen -W "${options[*]}" -- ${current}))
 }
 
 complete -F autocomplete ./src/fizzbuzz.sh
